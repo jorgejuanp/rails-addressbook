ba867c648e3a3d43c255d9dce335e174a0c2f7ef
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-12-05 16:50:52 +0100

    Add 'Create new contact' link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'contacts#index'
+  get '/contacts' => 'contacts#index'
   get '/contacts/new' => 'contacts#new'
   post '/contacts', to: 'contacts#create'
 end
