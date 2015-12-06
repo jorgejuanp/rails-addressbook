83b060930160d976fce6d665346fa6d55d32e1ed
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-12-06 04:59:40 +0100

    Iteration 7: Add validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-
+  validates_presence_of :name, :address, presence: true
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates :phone_number, numericality: { only_integer: true }
 end
